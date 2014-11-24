29b430e258466a2a6a9e5dac21a0621b61c043f4
jhinkel <jhinkel3@gmail.com>
2014-11-24 09:15:40 -0500

    Added new activity for upload

--- app/src/main/java/edu/gatech/travel/UploadActivity.java
+++ app/src/main/java/edu/gatech/travel/UploadActivity.java
@@ -1,28 +1,24 @@
 package edu.gatech.travel;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class UploadActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_upload);
     }
-    public void onUploadClick(View v){
-        startActivity(new Intent(getApplicationContext(),UploadActivity.class));
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_upload, menu);
         return true;
     }
 
