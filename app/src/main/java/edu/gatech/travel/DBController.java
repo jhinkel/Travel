db290ce0a4fd22fc64222c9413404a1eea437de8
jhinkel <jhinkel3@gmail.com>
2014-11-25 19:36:04 -0500

    Got DBController.java completely prepped and ready for inserting/polling lists.

--- app/src/main/java/edu/gatech/travel/DBController.java
+++ app/src/main/java/edu/gatech/travel/DBController.java
@@ -17,16 +17,24 @@ public class DBController  extends SQLiteOpenHelper {
     }
     //Creates Table
     @Override
+
     public void onCreate(SQLiteDatabase database) {
         String query;
-        query = "CREATE TABLE users ( userId INTEGER, userName TEXT)";
+        String query2;
+        query = "CREATE TABLE lists(ID integer NOT NULL AUTO_INCREMENT, title varchar(50), description varchar(500), latitude varchar(500), longitude varchar(500), achievementids varchar(1000), PRIMARY KEY(ID))";
+        query2 = "CREATE TABLE achievements(ID integer NOT NULL AUTO_INCREMENT, title varchar(50), imageLink varchar(500), latitude varchar(500), longitude varchar(500), radius varchar(5), description varchar(500), PRIMARY KEY(ID))";
         database.execSQL(query);
+        database.execSQL(query2);
     }
     @Override
     public void onUpgrade(SQLiteDatabase database, int version_old, int current_version) {
         String query;
-        query = "DROP TABLE IF EXISTS users";
+        String query2;
+        query = "DROP TABLE IF EXISTS lists";
+        query2 = "DROP TABLE IF EXISTS achievements";
+
         database.execSQL(query);
+        database.execSQL(query2);
         onCreate(database);
     }
 
@@ -34,35 +42,42 @@ public class DBController  extends SQLiteOpenHelper {
      * Inserts User into SQLite DB
      * @param queryValues
      */
-    public void insertUser(HashMap<String, String> queryValues) {
+
+    public void insertList(HashMap<String, String> queryValues) {
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put("userId", queryValues.get("userId"));
-        values.put("userName", queryValues.get("userName"));
-        database.insert("users", null, values);
+        values.put("title", queryValues.get("title"));
+        values.put("description", queryValues.get("description"));
+        values.put("latitude", queryValues.get("latitude"));
+        values.put("longitude", queryValues.get("longitude"));
+        values.put("achievements", queryValues.get("achievements"));
+
+        database.insert("lists", null, values);
         database.close();
     }
 
-    /**
-     * Get list of Users from SQLite DB as Array List
-     * @return
-     */
-    public ArrayList<HashMap<String, String>> getAllUsers() {
-        ArrayList<HashMap<String, String>> usersList;
-        usersList = new ArrayList<HashMap<String, String>>();
-        String selectQuery = "SELECT  * FROM users";
+
+
+    public ArrayList<HashMap<String, String>> getAllLists() {
+        ArrayList<HashMap<String, String>> ListOfLists;
+        ListOfLists = new ArrayList<HashMap<String, String>>();
+        String selectQuery = "SELECT  * FROM lists";
         SQLiteDatabase database = this.getWritableDatabase();
         Cursor cursor = database.rawQuery(selectQuery, null);
         if (cursor.moveToFirst()) {
             do {
                 HashMap<String, String> map = new HashMap<String, String>();
-                map.put("userId", cursor.getString(0));
-                map.put("userName", cursor.getString(1));
-                usersList.add(map);
+                map.put("title", cursor.getString(0));
+                map.put("description", cursor.getString(1));
+                map.put("latitude", cursor.getString(2));
+                map.put("longitude", cursor.getString(3));
+                map.put("achievements", cursor.getString(4));
+
+                ListOfLists.add(map);
             } while (cursor.moveToNext());
         }
         database.close();
-        return usersList;
+        return ListOfLists;
     }
 
 }
