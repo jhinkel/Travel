2f390ee9ff242f87258cfb9d770cb4b9f3d387fe
CrypticSplicer <splicer@homespliced.com>
2014-12-01 07:22:18 -0500

    Added share functionality to activity_complete_achievement.
    Created a dummy activity share2facebook first to figure it out. If you see it referenced anywhere delete it! I think I got rid of it completely though.

--- app/src/main/java/edu/gatech/travel/CompleteAchievement.java
+++ app/src/main/java/edu/gatech/travel/CompleteAchievement.java
@@ -1,19 +1,70 @@
 package edu.gatech.travel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.FacebookDialog;
 
 
 public class CompleteAchievement extends Activity {
+
+    private UiLifecycleHelper uiHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_achievement);
+
+        uiHelper = new UiLifecycleHelper(this, null);
+        uiHelper.onCreate(savedInstanceState);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() {
+            @Override
+            public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+                Log.e("Activity", String.format("Error: %s", error.toString()));
+            }
+
+            @Override
+            public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+                Log.i("Activity", "Success!");
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +86,12 @@ public class CompleteAchievement extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onShareClick(View v){
+        FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(this)
+                .setLink("https://developers.facebook.com/android")
+                .build();
+        uiHelper.trackPendingDialogCall(shareDialog.present());
     }
 }
