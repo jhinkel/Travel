959e3a54502ec1aaf91465646018ba34781a6d24
jhinkel <jhinkel3@gmail.com>
2014-12-05 18:48:51 -0500

    View achievement adapter works

--- app/src/main/java/edu/gatech/travel/AchievementList.java
+++ app/src/main/java/edu/gatech/travel/AchievementList.java
@@ -1,22 +1,41 @@
 package edu.gatech.travel;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class AchievementList extends Activity {
+    ArrayList<String> listItems=new ArrayList<String>();
+    achievementAdapter adapter;
+    DBController controller = new DBController(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement_list);
+
+        adapter=new achievementAdapter(this, listItems);
+
+        DBController database = new DBController(this);
+        ListView x = (ListView) this.findViewById(R.id.listView);
+        x.setAdapter(adapter);
+        ArrayList<HashMap<String,String>> temp = controller.getAllAchievements();
+        for(int i=0;i<temp.size();i++){
+            listItems.add(temp.get(i).get("id"));
+        }
+
     }
 
     public void onNewClick(View v){
-        //show list of new ones
+        startActivity(new Intent(getApplicationContext(), AddNewAchievement.class));
     }
 
     public void onExistingClick(View v){
